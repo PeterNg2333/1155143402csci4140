dab2037b26a78531f1318a64551ca09870486412
PeterNgKP <95013548+PeterNg2333@users.noreply.github.com>
2024-02-17 23:00:18 +0800

    test

---
+++ web/lib/db_test.php
@@ -0,0 +1,26 @@
+<?php
+$host = 'dpg-cn6tmn2cn0vc73dmghjg-a';
+$dbname = 'db_1155143402csci4140';
+$username = 'db_1155143402csci4140_user';
+$password = 'RQHfnfnO07Owiin69v9mf375Vrkd2yPi';
+
+try {
+    $conn = new PDO("pgsql:host=$host;port=5432;dbname=$dbname;user=$username;password=$password");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $conn->query('SELECT * from myuser;');
+    $version = $stmt->fetchColumn();
+    echo "<p>Output result: " . $version . "</p>";
+
+} catch(PDOException $e) {
+    echo "<p>Unable to connect to the database: " . $e->getMessage() . "</p>";
+}
+?>
+
+<!-- 
+
+
+    Create Table MyUser(id INT Primary Key, Name Varchar Not Null, hash_password Varchar NOT NULL, SALT Varchar NOT NULL,FLAG INTEGER NOT NULL) 
+    Create Table MyImage(img_id INT Primary Key, Name Varchar Not Null, img bytea, filetype Text, FLAG INTEGER NOT NULL)
+
+
+-->

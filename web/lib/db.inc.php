403c0ea8c3b2c49f318db1066516579aa3291752
PeterNgKP <95013548+PeterNg2333@users.noreply.github.com>
2024-02-17 22:57:22 +0800

    test

--- web/lib/db.inc.php
+++ web/lib/db.inc.php
@@ -9,7 +9,7 @@ $password = 'RQHfnfnO07Owiin69v9mf375Vrkd2yPi';
 try {
     $conn = new PDO("pgsql:host=$host;port=5432;dbname=$dbname;user=$username;password=$password");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $stmt = $conn->query('SELECT version()');
+    $stmt = $conn->query('SELECT * from myuser;');
     $version = $stmt->fetchColumn();
     echo "<p>Output result: " . $version . "</p>";
 

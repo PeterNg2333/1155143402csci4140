a126d7c099ad0be4aa0425a6a5d106b6fb9cf59d
PeterNgKP <95013548+PeterNg2333@users.noreply.github.com>
2024-02-19 01:44:31 +0800

    Update imageImg.php

--- web/imageImg.php
+++ web/imageImg.php
@@ -1,19 +1,17 @@
 <?php
 require __DIR__.'/lib/db_connect.php';
 
-
-
 $imageData = retrieve_image();
 $imageDataDecoded = base64_decode($imageData);
 
 $image = new Imagick();
-$image->setImageFormat('png');
 $image->readImageBlob($imageDataDecoded);
+$image->setImageFormat('png');
 
 $image -> blurImage(5, 3);
 $image -> borderImage('black', 5, 5);
 
 header('Content-type: ' . 'image/png');
-echo $image;
+echo $image->getImageBlob();
 
 ?>

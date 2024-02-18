8e9672efc230450676f053d287a6321a82a3245e
PeterNgKP <95013548+PeterNg2333@users.noreply.github.com>
2024-02-18 13:37:14 +0800

    Html part done

--- web/initialization.php
+++ web/initialization.php
@@ -1,21 +1,27 @@
 <!DOCTYPE html>
 <html>
-  <head>
-      <meta charset="utf-8"/>
-      <meta name="viewport" content="width=device-width, initial-scale=1"/>
-      <link rel="icon" type="image/x-icon" href="Resources/Instagram_icon.png"/>
-      <title>CSCI4140-Web Instagram-1155143402</title>
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous"/>
-      <link href="index.css" rel="stylesheet"/>
-  </head>
-  <body>
-    <?php
-      echo '<h1>Hello this world! 1.0 </h1>';
-      echo '<p>This page uses PHP version '
-          . phpversion()
-          . '.</p>';
-      include('lib/db_connect.php');
-      echo db_test();
-    ?> 
-  </body>
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link rel="icon" type="image/x-icon" href="Resources/Instagram_icon.png"/>
+        <title>CSCI4140-Web Instagram-1155143402</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous"/>
+    </head>
+    <body>
+        <main class="mt-5 container border border-dark">
+          <h3 class="text-center mx-auto my-3 loginHeader" style="font-weight: bold;"> System Initialization</h3>
+          <div class="text-center m-2">
+              <strong class="text-danger ">Important: </strong><span>all data would be deleted.</span>
+          </div>
+          <form class="mx-auto" method="POST" action="" class="row">
+              <div class="form-outline text-center mb-4">
+                  <button class="btn btn-success btn-block"> Please Go Ahead </button>
+                  <a href="./index.html" class="btn btn-danger btn-block"> Go Back </a>
+              </div>
+              <div class="form-outline text-center mb-4">
+                  
+              </div>
+          </form>
+        </smain>
+    </body>
 </html>
--- web/login.php
+++ web/login.php
@@ -6,17 +6,16 @@
       <link rel="icon" type="image/x-icon" href="Resources/Instagram_icon.png"/>
       <title>CSCI4140-Web Instagram-1155143402</title>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous"/>
-      <link href="index.css" rel="stylesheet"/>
   </head>
   <body>
       <main class="mt-5">
           <form class="mx-auto" method="POST" action="">
-              <h3 class="text-center mx-auto mb-4 loginHeader"> SIGN IN TO YOUR ACCOUNT</h3>
+              <h3 class="text-center mx-auto mb-4 loginHeader" style="font-weight: bold;"> SIGN IN TO YOUR ACCOUNT</h3>
               <div class="form-outline text-center  mb-4 mx-auto">  
-                  <input class="loginInput" id="username" name="username" placeholder="User name" required pattern="^[\w\-\/][\w\'\-\/\.]*@[\w\-]+(\.[\w\-]+)*(\.[\w]{2,6})$"/>
+                  <input style="width: 250px;" id="username" name="username" placeholder="User name" required pattern="^[\w\-\/][\w\'\-\/\.]*@[\w\-]+(\.[\w\-]+)*(\.[\w]{2,6})$"/>
               </div> 
               <div class="form-outline text-center  mb-4 mx-auto"> 
-                <input class="loginInput" type="password" id="password" name="password" placeholder="Password" pattern="^[\w\-\@\!\']+$" required/>
+                <input style="width: 250px;" type="password" id="password" name="password" placeholder="Password" pattern="^[\w\-\@\!\']+$" required/>
               </div>
 
               <!-- CSRF -->
@@ -25,7 +24,7 @@
               ?> 
 
               <div class="form-outline text-center mb-4 mx-auto">
-                <button class="btn btn-primary btn-block loginInput" > Login </button>
+                <button class="btn btn-primary btn-block" style="width: 250px;"> Login </button>
               </div>
           </form>
           <div class="form-outline text-center mb-4 mx-auto">

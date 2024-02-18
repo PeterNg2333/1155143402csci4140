b335b48bc53c01b63e1ffded12816c93cdbdea47
PeterNgKP <95013548+PeterNg2333@users.noreply.github.com>
2024-02-18 14:38:29 +0800

    Update initialization.php

--- web/initialization.php
+++ web/initialization.php
@@ -8,20 +8,27 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous"/>
     </head>
     <body>
-        <main class="mt-5 container border border-dark">
-          <h3 class="text-center mx-auto my-3 loginHeader" style="font-weight: bold;"> System Initialization</h3>
-          <div class="text-center m-2">
-              <strong class="text-danger ">Important: </strong><span>all data would be deleted.</span>
-          </div>
-          <form class="mx-auto" method="POST" action="" class="row">
-              <div class="form-outline text-center mb-4">
-                  <button class="btn btn-success btn-block"> Please Go Ahead </button>
-                  <a href="./index.html" class="btn btn-danger btn-block"> Go Back </a>
-              </div>
-              <div class="form-outline text-center mb-4">
-                  
-              </div>
-          </form>
-        </smain>
+        <main class="mt-5 container ">
+            <div class="row">
+                <div class="col-3"> </div>
+                <div class="col-6 mt-1 pt-1 border border-dark">
+                    <h3 class="text-center mx-auto my-3 loginHeader" style="font-weight: bold;"> System Initialization</h3>
+                    <div class="text-center m-2">
+                        <strong class="text-danger ">Important: </strong><span>all data would be deleted.</span>
+                    </div>
+                    <form class="mx-auto" method="POST" action="" class="row">
+                        <div class="form-outline text-center mb-4">
+                            <button class="btn btn-success btn-block"> Please Go Ahead </button>
+                            <a href="./index.html" class="btn btn-danger btn-block"> Go Back </a>
+                        </div>
+                        <div class="form-outline text-center mb-4">
+                            
+                        </div>
+                    </form> 
+                </div>
+                <div class="col-3"> </div>
+            </div>
+
+        </main>
     </body>
 </html>

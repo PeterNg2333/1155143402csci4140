986371e7f65ce500d56c3ff548583dd2d672fb3a
PeterNgKP <95013548+PeterNg2333@users.noreply.github.com>
2024-02-18 18:00:54 +0800

    create accound

--- web/lib/db_connect.php
+++ web/lib/db_connect.php
@@ -56,8 +56,23 @@ function csci4140_create_pd(){
     }
     $username = validate_input(string_sanitization($_REQUEST['username']), '/[^$@\'&"=|]+/', "invalid-username");
     $password = validate_input(string_sanitization($_REQUEST['password']), '/[^$@\'&"=|]+/', "invalid-password");
+    if (strpos($username, "user") > 0)
+        $flag = 0;
+    else if (strpos($username, "admin") > 0)
+        $flag = 1;
+    $salt = random_int(0, PHP_INT_MAX);
+    $hash_password = hash_hmac('sha256', $password, $salt);
 
-    $response = array('username' => $username, 'password' => $password);
-    return json_encode($response);
+    $query = $conn->prepare('INSERT INTO myuser(name, hash_password, salt, flag) VALUES (?, ?, ?, ?);');
+    $query -> bindColumn(1, $username);
+    $query -> bindColumn(2, $hash_password);
+    $query -> bindColumn(3, $salt);
+    $query -> bindColumn(4, $flag);
+
+    if ($query->execute()) {
+        return "Successfully created the accound for " . $username;
+    } else {
+        return "Error in query";
+    }
 }
 ?>

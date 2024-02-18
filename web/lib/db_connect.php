5a9b261a469c8e7cb29a801167b1201d1ef4873f
PeterNgKP <95013548+PeterNg2333@users.noreply.github.com>
2024-02-18 19:01:43 +0800

    Update db_connect.php

--- web/lib/db_connect.php
+++ web/lib/db_connect.php
@@ -57,10 +57,6 @@ function csci4140_create_pd(){
     $username = validate_input(string_sanitization($_REQUEST['username']), '/[^$@\'&"=|]+/', "invalid-username");
     $password = validate_input(string_sanitization($_REQUEST['password']), '/[^$@\'&"=|]+/', "invalid-password");
     $flag = 0;
-    if (strpos($username, "user"))
-        $flag = 0;
-    else
-        $flag = 1;
     $salt = random_int(0, PHP_INT_MAX);
     $hash_password = hash_hmac('sha256', $password, $salt);
 

150dad2fc23a74e3fd8194d82429547247de01a3
PeterNgKP <95013548+PeterNg2333@users.noreply.github.com>
2024-02-18 15:32:45 +0800

    debug 2

--- web/lib/utilities/validation.php
+++ web/lib/utilities/validation.php
@@ -10,6 +10,7 @@ function is_form_action($action){
         || $action == "save_image" 
         || $action == "delete_image" 
         || $action == "delete_all"
+        || $action == "db_test"
         ){
         return true;
     }

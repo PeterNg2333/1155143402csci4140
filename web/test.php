690273df4b22fa2ac91b01e5fbf1c6e3efac01c1
PeterNgKP <95013548+PeterNg2333@users.noreply.github.com>
2024-02-19 14:43:15 +0800

    Update test.php

--- web/test.php
+++ web/test.php
@@ -4,9 +4,22 @@
         $length = (int) $_GET['len'] ?? 1;
         // $username = is_auth();
         echo count_image("admin01");
-        echo json_encode(fetch_ten_public_image($start, $length));
-        echo "\n";
-        echo json_encode(fetch_ten_image_auth($start, $length, "user01"));
-        echo json_encode(fetch_ten_image_auth($start, $length, "admin01"));
+        $res = fetch_ten_public_image($start, $length);
+        foreach ($res as $image){
+            echo '<figcaption class="col-3 d-block">';
+            echo '    <div class="card">';
+            echo '        <img src="./lib/image.php?img_id='.$image['img_id'].'" class="card-img-top" alt="Sunset over the Sea"/>';
+            echo '        <div class="card-body container py-2">';
+            echo '            <div class="row">';
+            echo '                <span class="card-title col-8">'.$image['img_id'].'</span>';
+            echo '                <a href="'."./lib/image.php?img_id=". $image['img_id'].'"class="btn btn-sm btn-secondary col-4">Edit</a>';
+            echo '            </div>';
+
+            echo '        </div>';
+            echo '    </div>';
+            echo '</figcaption>';
+        }
+
+        
             
 ?> 

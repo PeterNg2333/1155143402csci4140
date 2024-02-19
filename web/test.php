cf760c9dc8a0e2d2984208fccb45250272c562a7
PeterNgKP <95013548+PeterNg2333@users.noreply.github.com>
2024-02-19 12:58:20 +0800

    Update test.php

--- web/test.php
+++ web/test.php
@@ -3,6 +3,6 @@
         $start = (int) $_GET['start'] ?? 0;
         $length = (int) $_GET['len'] ?? 1;
         $username = is_auth();
-        echo count_image($username);
+        // echo count_image($username);
         echo json_encode(fetch_ten_public_image($start, $length));
 ?> 

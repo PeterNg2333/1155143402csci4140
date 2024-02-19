a1e6ebffa93f7a04e0ab43e2c6179f9002c8505b
PeterNgKP <95013548+PeterNg2333@users.noreply.github.com>
2024-02-19 20:51:17 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -71,8 +71,8 @@
                     $res = fetch_ten_public_image($start, $length);
                     $res = (array) array_slice($res, $start, $length);
                         foreach ($res as $image){
-                            echo '<figcaption class="col-3 d-block mb=2">';
-                            echo '    <div class="card">';
+                            echo '<figcaption class="col-3 d-block mb-2">';
+                            echo '    <div class="card w-100 h-100">';
                             echo '        <img src="./lib/image.php?img_id='.$image['img_id'].'" class="card-img-top" alt="Sunset over the Sea"/>';
                             echo '        <div class="card-body container py-2">';
                             echo '            <div class="row">';

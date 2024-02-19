ef51b2d891982f127329d00c6dedd5810bdbd7bc
PeterNgKP <95013548+PeterNg2333@users.noreply.github.com>
2024-02-19 20:22:35 +0800

    Update db_connect.php

--- web/lib/db_connect.php
+++ web/lib/db_connect.php
@@ -181,7 +181,7 @@ function fetch_ten_image_auth($start, $length, $username){
 }
                                                      
 function csci4140_finish_edit(){
-    echo json_decode($_GET);
+    echo json_encode($_GET);
     return 0;
     if (isset($_GET['img_id']) && isset($_GET['filter'])){
         $img_id = validate_input(int_sanitization($_POST['img_id'] ), '/^\d+$/', "invalid-img_id");
